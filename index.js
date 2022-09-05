1f801fd6b73db03719baa29b353d76592b0485a2
BLACK <jonanda@gmx.de>
2022-09-05 18:13:40 +0200

    more style updates, added status bar, accessibility

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function setup() {
     var dateElem = document.getElementById("date")
     dateElem.min = new Date().toISOString().slice(0, -8)
-    dateElem.value = new Date(Date.now() + 3 * 24 * 60 * 60 * 1000).toISOString().slice(0, -8)
+    dateElem.value = new Date(Date.now()).toISOString().slice(0, -8)
 
     const formElem = document.getElementById("createForm")
     formElem.addEventListener("submit", e => {
@@ -15,6 +15,18 @@ function createURL() {
     const shorturl = document.getElementById("url").value
     const name = document.getElementById("name").value
     const date = document.getElementById("date").value
+    if(!shorturl){
+        document.getElementById("response").innerHTML = "Bitte gib eine URL an"
+        return
+    }
+    if(!shorturl.startsWith("https://") && !shorturl.startsWith("http://")){
+        document.getElementById("response").innerHTML = "Bitte gib eine gültige URL an"
+        return
+    }
+    /*if(!name){
+        document.getElementById("response").innerHTML = "Bitte gib einen Namen an"
+        return
+    }*/
 
     var params = JSON.stringify({url: shorturl, name, date})
     var xhr = new XMLHttpRequest()
